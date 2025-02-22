6caa3d2413dedfa586d9d7652ccc85efe5a0a74c
Mahab <malmezayyen@outlook.com>
2025-02-22 09:36:19 +0300

    Commit implementation course interfaces
    Implement course srvice and repository concreate classes.

---
+++ Cleanarch/Cleanarch.Application/Services/Course.cs
@@ -0,0 +1,28 @@
+﻿using Cleanarch.Application.Interfaces;
+using Cleanarch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleanarch.Application.Services
+{
+    public class Course : Icourseservice
+    {
+        private readonly Icourserepository _Courserepository;
+
+        public Course(Icourserepository courserepository)
+        {
+            _Courserepository=courserepository;
+        }
+
+        public Viewmodels.Course Getcourses()
+        {
+            return new Viewmodels.Course
+            {
+                Courses = _Courserepository.Getcourses()
+            };
+        }
+    }
+}
---
+++ Cleanarch/Cleanarch.Infra.Data/Repositories/Course.cs
@@ -0,0 +1,25 @@
+﻿using Cleanarch.Domain.Interfaces;
+using Cleanarch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleanarch.Infra.Data.Repositories
+{
+    public class Course : Icourserepository
+    {
+        private readonly Universitydbcontext _ctx;
+
+        public Course(Universitydbcontext ctx)
+        {
+            _ctx=ctx;
+        }
+
+        public IEnumerable<Domain.Models.Course> Getcourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
