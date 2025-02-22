ef27a742c8960ec18f2036f296334617d897e1e3
Mahab <malmezayyen@outlook.com>
2025-02-22 09:24:48 +0300

    Commit course code
    Commit view model, service interface, and repository interface of course.

---
+++ Cleanarch/Cleanarch.Application/Interfaces/Icourseservice.cs
@@ -0,0 +1,14 @@
+﻿using Cleanarch.Application.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleanarch.Application.Interfaces
+{
+    public interface Icourseservice
+    {
+        Viewmodels.Course Getcourses();
+    }
+}
---
+++ Cleanarch/Cleanarch.Application/Viewmodels/Course.cs
@@ -0,0 +1,14 @@
+﻿using Cleanarch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleanarch.Application.Viewmodels
+{
+    public class Course
+    {
+        public IEnumerable<Domain.Models.Course> Courses { get; set; }
+    }
+}
